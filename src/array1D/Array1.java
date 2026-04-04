36949ffedbae1b284809e3a556b1ad39e5516690
Sknaseebkamal <sknaseebkamal@gmail.com>
2026-04-04 14:12:23 +0530

    initial commit

---
+++ src/array1D/Array1.java
@@ -0,0 +1,18 @@
+package array1D;
+
+import java.util.Arrays;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a[];
+a=new int[10];
+System.out.println(a.length);
+
+a[0]=10;
+a[6]=12;
+System.out.println(Arrays.toString(a));
+	}
+
+}
