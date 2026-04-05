898aa14bba5e9d1065c0dc674602d36314aaeb76
Sknaseebkamal <sknaseebkamal@gmail.com>
2026-04-05 08:49:28 +0530

    Added Array2.java

---
+++ src/array1D/Array2.java
@@ -0,0 +1,13 @@
+package array1D;
+
+import java.util.Arrays;
+
+public class Array2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer a[]=new Integer[10];//Array of objects of Integer type
+		System.out.println(Arrays.toString(a));
+	}
+
+}
