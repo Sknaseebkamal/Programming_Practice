e010946bcb3599043be8c31e09412380492e2c73
Sknaseebkamal <sknaseebkamal@gmail.com>
2026-04-10 11:38:39 +0530

    Added file

---
+++ src/array1D/Array8reverse.java
@@ -0,0 +1,14 @@
+package array1D;
+
+public class Array8reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n[]= {9,2,5,1,3};
+		for(int i=n.length-1;i>=0;i--) {
+			System.out.println(n[i]);
+		}
+
+	}
+
+}
