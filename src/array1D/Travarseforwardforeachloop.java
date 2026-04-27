360f0f3eaa2229847fa02b5af62217aa508f620a
Sknaseebkamal <sknaseebkamal@gmail.com>
2026-04-27 17:22:13 +0530

    file add

---
+++ src/array1D/Travarseforwardforeachloop.java
@@ -0,0 +1,12 @@
+package array1D;
+
+public class Travarseforwardforeachloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]= {1,2,3,4,5,6};
+		for(int x:a)
+		System.out.println(x );
+	}
+
+}
