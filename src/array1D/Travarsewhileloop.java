4c32a19ce524f1d3c859aada426ad7eb08567e9f
Sknaseebkamal <sknaseebkamal@gmail.com>
2026-04-28 07:10:12 +0530

    file add

---
+++ src/array1D/Travarsewhileloop.java
@@ -0,0 +1,15 @@
+package array1D;
+
+public class Travarsewhileloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]= {6,5,4,3,22,4};
+		int i=0;
+		while(i<a.length) {
+			System.out.println(a[i]);
+			i++;
+		}
+	}
+
+}
