7b03c749ad4c3628d7941e88ff6af90c39eb70cb
Sknaseebkamal <sknaseebkamal@gmail.com>
2026-04-29 13:35:08 +0530

    file add

---
+++ src/array1D/Traverseforeach.java
@@ -0,0 +1,15 @@
+package array1D;
+
+import java.util.Set;
+
+public class Traverseforeach {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Set<Integer> set=Set.of(9,3,6,2,0);
+		for(int x:set) {
+			System.out.println(x );
+		}
+	}
+
+}
